020bb4d44b083e3c645e2022001a4dd633feeb47
Natalia <nlopezsalas@gmail.com>
2024-03-09 11:58:54 +0000

    API with flask

--- src/datastructures.py
+++ src/datastructures.py
@@ -8,29 +8,52 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
-class FamilyStructure:
+class Family:
     def __init__(self, last_name):
         self.last_name = last_name
-
-        # example list of members
-        self._members = []
+        self._members = [
+            {"id": self._generateId(), "first_name": "John", "last_name": self.last_name, "age": 33, "lucky_numbers": [7, 13, 22]},
+            {"id": self._generateId(), "first_name": "Jane", "last_name": self.last_name, "age": 35, "lucky_numbers": [10, 14, 3]},
+            {"id": self._generateId(), "first_name": "Jimmy", "last_name": self.last_name, "age": 5, "lucky_numbers": [1]}
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
+    # def add_member(self, member):
+    #     self._members.append(member)
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        if "id" not in member:
+            member["id"] = self._generateId()
+        member["last_name"] = self.last_name  # Ensure the last name is Jackson
+        self._members.append(member)
 
     def delete_member(self, id):
+        self._members = [member for member in self._members if member['id'] != id]
+        
         # fill this method and update the return
-        pass
+        # member_pos = None
+        # for pos in range(0, len(self._members)):
+        #     if self._members[pos]["id"] == id:
+        #         member_pos = pos
+        # if member_pos != None: 
+        #     self._members.pop(member_pos)
+        #     return 'ok'
+        # else: return 'no existe el id'
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        member = list(filter(lambda member: member["id"] == id ,self._members))
+        if member != []:
+            return member[0]
+        else: return None
 
+        # for pos in range(0, len(self._members)):
+        #     if self._members[pos]["id"] == id:
+        #         return self._members[pos]
+            
+        # return None
+    
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
-        return self._members
+       return self._members
